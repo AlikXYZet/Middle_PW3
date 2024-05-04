622457383d8bd392027daeed6eb68db9c8185ca9
Александр Осипов <alikxyzet@gmail.com>
2024-05-04 20:45:37 +0300

    1.3 (PW) Save All

--- M1.1.cpp
+++ M1.1.cpp
@@ -23,13 +23,13 @@ public:
 		return sqrt(x * x + y * y + z * z);
 	}
 
-	friend Vector operator+(const Vector& a, const Vector& b);
+	friend Vector operator + (const Vector& a, const Vector& b);
 
-	friend std::ostream& operator<<(std::ostream& out, const Vector& v);
+	friend std::ostream& operator << (std::ostream& out, const Vector& v);
 
-	friend bool operator>(const Vector& a, const Vector& b);
+	friend bool operator > (const Vector& a, const Vector& b);
 
-	float operator[](int index)
+	float operator [] (int index)
 	{
 		switch (index)
 		{
@@ -49,34 +49,94 @@ public:
 		}
 	}
 
+	// ================================== Practical Work ================================
+	Vector operator * (const float& a)
+	{
+		return Vector(x * a, y * a, z * a);
+	};
+
+	Vector operator *= (const float& a)
+	{
+		*this = *this * a;
+		return *this;
+	};
+
+	friend Vector operator - (const Vector& a, const Vector& b);
+
+	friend std::istream& operator >> (std::istream& in, Vector& v);
+	// ==================================================================================
+
 private:
 	float x;
 	float y;
 	float z;
 };
 
-Vector operator+(const Vector& a, const Vector& b)
+Vector operator  +(const Vector& a, const Vector& b)
 {
 	return Vector(a.x + b.x, a.y + b.y, a.z + b.z);
 }
 
-std::ostream& operator<<(std::ostream& out, const Vector& v)
+std::ostream& operator << (std::ostream& out, const Vector& v)
 {
 	out << v.x << ' ' << v.y << ' ' << v.z;
 	return out;
 }
 
-bool operator>(const Vector& a, const Vector& b)
+bool operator > (const Vector& a, const Vector& b)
 {
 	return false;
 }
 
+// ================================== Practical Work ================================
+Vector operator - (const Vector& a, const Vector& b)
+{
+	return Vector(a.x - b.x, a.y - b.y, a.z - b.z);
+}
+
+std::istream& operator >> (std::istream& in, Vector& v)
+{
+	in >> v.x;
+	in >> v.y;
+	in >> v.z;
+	return in;
+};
+// ==================================================================================
+
 int main()
 {
 	Vector v1(0, 1, 2);
-	Vector v2(3, 4, 5);
+	Vector v2(3, 5, 7);
 	Vector v3;
 	v3 = v1 + v2;
 	std::cout << v3 << '\n';
-	std::cout << "v3 lenght " << static_cast<float>(v3);
+	std::cout << "v3 lenght " << static_cast<float>(v3) << '\n';
+	std::cout << '\n';
+
+	// ================================== Practical Work ================================
+	std::cout << "v1: " << v1 << '\n';
+	std::cout << "v2: " << v2 << '\n';
+	std::cout << "v3: " << v3 << '\n';
+	std::cout << '\n';
+
+	// Operator *
+	float a1 = 10;
+	v3 = v1 * a1;
+	std::cout << "Test operator* : " << v3 << '\n';
+	std::cout << "v1: " << v1 << '\n';
+	std::cout << '\n';
+
+	v3 *= a1;
+	std::cout << "Test operator*= : " << v3 << '\n';
+	std::cout << '\n';
+
+	// Operator -
+	v3 = v2 - v1;
+	std::cout << "Test operator- : " << v3 << '\n';
+	std::cout << '\n';
+
+	// std::cin
+	std::cin >> v3;
+	std::cout << "Test std::cin : " << v3 << '\n';
+	// ==================================================================================
 }
